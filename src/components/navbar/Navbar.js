e3e36c850289e30f13262482acb14aeb5f5dd248
Prime-02 <163609502+Prime-02@users.noreply.github.com>
2024-11-13 17:34:34 +0100

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
   return (
     <div className='fixed top-0 w-full pt-6  flex flex-row justify-between z-50 text-white px-10 items-center'>
         <span className='hover:text-neutral-500 cursor-pointer transition'>logo</span>
-        <span className='hover:text-neutral-500 cursor-pointer transition'>
-            Home
+        <span className='hover:text-neutral-500 cursor-pointer transition capitalize'>
+            Who we are
         </span>
         <span>
             <ButtonOne buttonValue={'Shop'} iconValue={(<IoMdCart size={18}/>)}/>
