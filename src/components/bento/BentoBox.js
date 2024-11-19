f6d2e9a368a614ad807909ce0aae646904e803da
Prime-02 <163609502+Prime-02@users.noreply.github.com>
2024-11-19 16:29:37 +0100

    .

--- src/components/bento/BentoBox.js
+++ src/components/bento/BentoBox.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react'
 import { BentoImg } from '../index'
 import Image from 'next/image'
-import styles from './BentoBox.module.css' // Assuming you are using CSS Modules
 import { ButtonTwo } from '../reusables/buttons/Buttons'
 
 const BentoBox = () => {
     const [seeMore, setSeeMore] = useState(false)
   return (
-    <div className={`${styles.container} ${seeMore ? 'h-screen' : 'h-auto'} overflow-hidden transition duration-500 relative`} >
-      <div className={styles.grid}>
+    <div className={` ${seeMore ? 'h-screen' : 'h-auto'} overflow-hidden transition duration-500 relative`} >
+      <div>
         {
           BentoImg.map((img, ind) => (
-            <div className={`${styles.imageWrapper} overflow-hidden rounded-xl`} key={ind}>
-              <Image src={img.img} className={`${styles.image} hover:scale-110 transition duration-300`} alt={`image ${ind + 1}`} loading='lazy'/> 
+            <div className={` overflow-hidden rounded-xl`} key={ind}>
+              <Image src={img.img} className={` hover:scale-110 transition duration-300`} alt={`image ${ind + 1}`} loading='lazy'/> 
             </div>
           ))
         }
