da109b351af048929641c77bb6beff965d6f1ae4
Prime-02 <163609502+Prime-02@users.noreply.github.com>
2024-11-20 09:50:09 +0100

    Update BentoBox.js

--- src/components/bento/BentoBox.js
+++ src/components/bento/BentoBox.js
@@ -1,24 +1,17 @@
 import React, { useState } from 'react'
 import { BentoImg } from '../index'
 import Image from 'next/image'
-import { ButtonTwo } from '../reusables/buttons/Buttons'
 
 const BentoBox = () => {
-    const [seeMore, setSeeMore] = useState(false)
   return (
-    <div className={` ${seeMore ? 'h-screen' : 'h-auto'} overflow-hidden transition duration-500 relative`} >
-      <div>
+      <div className='flex w-full items-center  h-[132px] overflow-clip mb-10'>
         {
           BentoImg.map((img, ind) => (
-            <div className={` overflow-hidden rounded-xl`} key={ind}>
+            <div className={` overflow-hidden rounded-xl `} key={ind}>
               <Image src={img.img} className={` hover:scale-110 transition duration-300`} alt={`image ${ind + 1}`} loading='lazy'/> 
             </div>
           ))
         }
-      </div>
-      <span className='absolute bottom-0 flex  w-full pb-2 pt-16 items-center justify-center bg-gradient-to-t from-gray-500'>
-        <ButtonTwo Clicked={()=>setSeeMore(!seeMore)} buttonValue={`See ${seeMore ? 'More' : 'Less'}`}/>
-      </span>
     </div>
   )
 }
